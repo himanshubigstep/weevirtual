78e206413e59e833317b6aecf9309dcb72d67242
himanshu <himanshu.singh@bigsteptech.com>
2020-06-20 14:13:23 +0530

    lounfe_component_conflict-error

--- src/app/shared/services/event.service.ts
+++ src/app/shared/services/event.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { Observable, Subject, BehaviorSubject, Observer } from 'rxjs';
 import { Router } from '@angular/router';
 import { HttpService } from './http.service';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable({
@@ -10,13 +11,15 @@ import { HttpService } from './http.service';
 export class EventService {
 
   private event_details_sub = new BehaviorSubject<any>(null);//-----------Subject variables
+  private videoStatus = new BehaviorSubject<any>(null);
+  public messages: Subject<string>;
   event_details = [];
   interval;
   timerObj = {};
 
   private readonly getPanellistByEventIdApi = 'events/panellist-list/';
 
-  constructor(private router: Router, private httpService: HttpService) { }
+  constructor(private router: Router, private httpService: HttpService,private httpClient:HttpClient) { }
 
   get eventDetails_Update(): Observable<boolean> {
     if (localStorage.getItem('details')) {
@@ -39,7 +42,7 @@ export class EventService {
   }
   timer() {
     let Days;
-    let start = "2020-05-29";
+    let start = "2020-06-01";
     let end = "2020-05-06";
     let present_Date = new Date;
     let present_time;
@@ -66,9 +69,62 @@ getVedioStreaminStatus(usertype,eventId){
     user_type:usertype,
     event_id : eventId
   }
-  return this.httpService.post('/events/video-stream-status/',body);
+  return this.httpService.post('events/video-stream-status/',body);
 }
   getPanellistByEventId(obj) {
     return this.httpService.post(this.getPanellistByEventIdApi, obj).toPromise();
   }
+//   setsock() {
+  
+//     let socket = new WebSocket('ws://15.206.20.212:8000/ws/4/');
+//     console.log('WebSockets connection created.');
+// socket.onopen= function(e){
+//   console.log("websocket open",e);
+//   socket.send(JSON.stringify({'event':'4'}));
+// };
+// socket.onmessage = function(data) {
+//       console.log("data from socket:" + data);
+
+//     };
+
+
+//   }
+   connect(): Subject<MessageEvent> {
+    let ws = new WebSocket('wss://dev-api.weevirtuall.com/ws/4/');
+    ws.onopen= function(e){
+        console.log("websocket open",e);
+        ws.send(JSON.stringify({'event':'3'}));
+      };
+    let observable = Observable.create(
+      (obs: Observer<MessageEvent>) => {
+        ws.onmessage = obs.next.bind(obs);
+        ws.onerror = obs.error.bind(obs);
+        ws.onclose = obs.complete.bind(obs);
+        return ws.close.bind(ws);
+      })
+    let observer = {
+      next: (data: string) => {
+        if (ws.readyState === WebSocket.OPEN) {
+          ws.send(data);
+          if(data=="stop")
+            ws.close(1000,"bye")
+        }
+      }
+    }
+    return Subject.create(observer, observable);
+  }
+ 
+ videoStatusUpdateFromAdmin(userType,event_id){
+   let body ={
+     user_type:userType,
+     event_id:event_id
+   }
+   let token = localStorage.getItem('token');
+if(userType == 'panellist'){
+return this.httpClient.post('https://dev-api.weevirtuall.com/events/panellist-video-status/',body,{headers:{'Authorization' :token}});
+}
+if(userType == 'attendee'){
+  return this.httpClient.post('https://dev-api.weevirtuall.com/events/attendee-video-status/',body,{headers:{'Authorization' :token}});
+  }
+ }
 }
